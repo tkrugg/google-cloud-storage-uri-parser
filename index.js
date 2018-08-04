d888636d34946a778d988bdaf76a451f317141ee
Youcef Mammar <y.mammar.dev@gmail.com>
2018-08-04 11:15:28 +0200

    added prettier

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const REG_EXP = new RegExp('^gs://([^/]+)/(.+)$');
+const REG_EXP = new RegExp("^gs://([^/]+)/(.+)$");
 const parse = uri => {
-    const bucket = uri.replace(REG_EXP, '$1')
-    const file = uri.replace(REG_EXP, '$2') 
-    return {
-        uri,
-        bucket,
-        file
-    }
-}
+  const bucket = uri.replace(REG_EXP, "$1");
+  const file = uri.replace(REG_EXP, "$2");
+  return {
+    uri,
+    bucket,
+    file
+  };
+};
 module.exports = {
-    parse
-}
+  parse
+};
--- tests/parse.test.js
+++ tests/parse.test.js
@@ -1,11 +1,12 @@
-const {
-    parse
-} = require('../');
-describe('parse', () => {
-    const tests = ["gs://mybucket/myfile.json", "gs://my-bucket_with/myfolder/myfile.json", ]
-    for (const test of tests) {
-        it(`should produce the right bucket and file names for ${test}`, () => {
-            expect(parse(test)).toMatchSnapshot()
-        })
-    }
+const { parse } = require("../");
+describe("parse", () => {
+  const tests = [
+    "gs://mybucket/myfile.json",
+    "gs://my-bucket_with/myfolder/myfile.json"
+  ];
+  for (const test of tests) {
+    it(`should produce the right bucket and file names for ${test}`, () => {
+      expect(parse(test)).toMatchSnapshot();
+    });
+  }
 });
